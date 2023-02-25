ad24dbce721daa8f46932db989a0714bf2067cf3
joneilson <joneilson84@gmail.com>
2023-02-25 11:28:27 -0300

    feat: search chat

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const menuItems = document.querySelectorAll('.menu-item');
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
 
 // =============== SIDEBAR =============
 
@@ -32,6 +34,24 @@ menuItems.forEach(item => {
 
 
 // =============== MESSAGES =============
+// search chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase()
+    console.log(val)
+    message.forEach(chat => {
+        let name = chat.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+// search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+// Hightlight messages card when messages menu item is clicked
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count'). style.display = 'none';
